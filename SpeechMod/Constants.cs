3f0f52555255e43e09fdfc259c377883af53f05a
Osmodium <schubertchr@gmail.com>
2022-01-24 18:36:22 +0100

    [Squash] Minor cleanup

--- SpeechMod/Constants.cs
+++ SpeechMod/Constants.cs
@@ -5,4 +5,3 @@ public static class Constants
     public const string WINDOWS_VOICE_NAME = "WindowsVoice";
     public const string ARROW_BUTTON_PATH = "DialogPCView/Body/View/Scroll View/ButtonEdge";
 }
-
