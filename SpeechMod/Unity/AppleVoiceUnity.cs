a8ec917b0c5d64e346231f3b216e042fdf1ac745
Osmodium <schubertchr@gmail.com>
2022-01-25 21:56:41 +0100

    Fix speech process reference check

--- SpeechMod/Unity/AppleVoiceUnity.cs
+++ SpeechMod/Unity/AppleVoiceUnity.cs
@@ -16,12 +16,20 @@ namespace SpeechMod.Unity
             else
                 m_TheVoice = this;
         }
-
+        
         public static void Speak(string text)
         {
+            if (m_TheVoice == null)
+            {
+                Main.Logger.Critical("No voice initialized!");
+                return;
+            }
+
             text = new Regex("<[^>]+>").Replace(text, "");
             //Process.Start("/usr/bin/killall", "say");
-            m_TheVoice.speechProcess?.Kill();
+            if (m_TheVoice.speechProcess is { HasExited: false })
+                m_TheVoice.speechProcess.Kill();
+
             string arguments = string.Concat("-v ", Main.ChosenVoice, " -r ", Main.Settings.Rate.ToString(), " ", text.Replace("\"", ""));
             m_TheVoice.speechProcess = Process.Start("/usr/bin/say", arguments);
         }
