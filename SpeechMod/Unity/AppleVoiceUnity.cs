01f8074e8c96ef7928a6465fee635d772ed41fee
Osmodium <schubertchr@gmail.com>
2022-01-30 11:53:55 +0100

    Fix swapped voices on macOS

--- SpeechMod/Unity/AppleVoiceUnity.cs
+++ SpeechMod/Unity/AppleVoiceUnity.cs
@@ -11,14 +11,8 @@ namespace SpeechMod.Unity
     {
         private static AppleVoiceUnity m_TheVoice;
         private Process m_SpeechProcess;
-
-        void Start()
-        {
-            if (m_TheVoice != null)
-                Destroy(gameObject);
-            else
-                m_TheVoice = this;
-        }
+        private static string GenderVoice => Game.Instance?.DialogController?.CurrentSpeaker?.Gender == Gender.Female ? Main.FemaleVoice : Main.MaleVoice;
+        private static int GenderRate => Game.Instance?.DialogController?.CurrentSpeaker?.Gender == Gender.Female ? Main.Settings.FemaleRate : Main.Settings.MaleRate;
 
         private static bool IsVoiceInitialized()
         {
@@ -29,9 +23,14 @@ namespace SpeechMod.Unity
             return false;
         }
 
-        private static string GenderVoice => Game.Instance?.DialogController?.CurrentSpeaker?.Gender == Gender.Female ? Main.FemaleVoice : Main.MaleVoice;
-        private static int GenderRate => Game.Instance?.DialogController?.CurrentSpeaker?.Gender == Gender.Female ? Main.Settings.FemaleRate : Main.Settings.MaleRate;
-
+        void Start()
+        {
+            if (m_TheVoice != null)
+                Destroy(gameObject);
+            else
+                m_TheVoice = this;
+        }
+        
         public static void Speak(string text, float delay = 0f)
         {
             if (!IsVoiceInitialized())
@@ -71,20 +70,20 @@ namespace SpeechMod.Unity
                 {
                     string argumentsPart = text.Substring(0, position);
                     text = text.Substring(position);
-                    arguments = $"{arguments}say -v {Main.NarratorVoice} -r {Main.Settings.NarratorRate} {argumentsPart.Replace("\"", "")};";
+                    arguments = $"{arguments}say -v  {GenderVoice} -r {GenderRate} {argumentsPart.Replace("\"", "")};";
                 }
                 else
                 {
                     position = text.IndexOf("</color>", StringComparison.InvariantCultureIgnoreCase);
                     string argumentsPart2 = text.Substring(0, position);
                     text = text.Substring(position);
-                    arguments = $"{arguments}say -v {GenderVoice} -r {GenderRate} {argumentsPart2.Replace("\"", "")};";
+                    arguments = $"{arguments}say -v {Main.NarratorVoice} -r {Main.Settings.NarratorRate} {argumentsPart2.Replace("\"", "")};";
                 }
             }
 
             text = text.Replace("\"", "");
-            if(!string.IsNullOrWhiteSpace(text))
-                arguments = $"{arguments}say -v {Main.NarratorVoice} -r {Main.Settings.NarratorRate} {text};";
+            if (!string.IsNullOrWhiteSpace(text) && text != "</color>")
+                arguments = $"{arguments}say -v {GenderVoice} -r {GenderRate} {text};";
 
             arguments = new Regex("<[^>]+>").Replace(arguments, "");
 
