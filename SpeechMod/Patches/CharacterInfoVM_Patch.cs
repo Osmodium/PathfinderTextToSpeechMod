640319478bedeea59588384c2be1ad7b596b5ffa
Osmodium <schubertchr@gmail.com>
2022-04-25 23:46:43 +0200

    Add TTS to story under summary and biography

---
+++ SpeechMod/Patches/CharacterInfoVM_Patch.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using Kingmaker.UI.MVVM._PCView.ServiceWindows.CharacterInfo;
+using Kingmaker.UI.MVVM._PCView.ServiceWindows.CharacterInfo.Sections.Stories;
+using Kingmaker.UI.MVVM._VM.ServiceWindows.CharacterInfo;
+using SpeechMod.Unity;
+using UnityEngine;
+
+namespace SpeechMod.Patches;
+
+[HarmonyPatch(typeof(CharacterInfoVM), "OnPageSelected")]
+public static class CharacterInfoVM_Patch
+{
+    public static void Postfix(CharInfoPageType pageType)
+    {
+        if (!Main.Enabled)
+            return;
+
+#if DEBUG
+        Debug.Log($"{nameof(CharacterInfoVM)}_OnPageSelected_Postfix - {pageType}");
+#endif
+
+        if (pageType is not (CharInfoPageType.SummaryPC or CharInfoPageType.BiographyPC))
+            return;
+
+        var stories = UIHelper.GetUICanvas()?.GetComponentsInChildren<CharInfoStoriesPCView>();
+
+        for (int i = 0; i < stories.Length; ++i)
+        {
+            var story = stories[i];
+            var textBox = story?.transform?.Find("StoryFull/StoryContent/TextBox");
+            if (textBox == null)
+            {
+                Debug.LogWarning($"{nameof(CharacterInfoVM)}_OnPageSelected_Postfix - {nameof(textBox)} not found for pagetype {pageType}!");
+                continue;
+            }
+
+            textBox.HookupTextToSpeechOnTransform();
+        }
+    }
+}
