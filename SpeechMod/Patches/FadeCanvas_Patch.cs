0507321273ded37702921645bc0d532b95698bc3
Osmodium <schubertchr@gmail.com>
2022-06-12 01:24:16 +0200

    Experiment with progressbar from static canvas

---
+++ SpeechMod/Patches/FadeCanvas_Patch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Kingmaker;
+using Kingmaker.UI;
+using UnityEngine;
+
+namespace SpeechMod.Patches;
+
+[HarmonyPatch(typeof(FadeCanvas), "Initialize")]
+public class FadeCanvas_Patch
+{
+    public static void Postfix()
+    {
+        if (!Main.Enabled)
+            return;
+
+        var sceneName = Game.Instance.CurrentlyLoadedArea.ActiveUIScene.SceneName;
+
+#if DEBUG
+        Debug.Log($"{nameof(FadeCanvas)}_Initialize_Postfix @ {sceneName}");
+#endif
+        
+        PlaybackControl.TryInstantiate();
+    }
+}
